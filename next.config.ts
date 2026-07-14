d02080d8cb596ff1af7174c28a591530b13750f1
CoderManYash <butlayash@gmail.com>
2026-07-14 20:14:31 +0530

    config: allow images from res.cloudinary.com

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
